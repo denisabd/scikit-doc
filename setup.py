5673755acb79bb64badb72da0830483532909bd2
Denis Abdullin <denisabdullincz@gmail.com>
2023-04-16 11:13:45 +0200

    src reworked

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 with open('README.md', 'r') as fh:
@@ -6,26 +6,22 @@ with open('README.md', 'r') as fh:
   
 setup(
   name = 'skdoc',
-  version = '0.0.1.2',
-  author = ['Denis Abdullin', 'Emile Antat'],
-  author_email = 'denisabdullincz@gmail.com',
+  version = '0.0.1.4',
+  author = 'Denis Abdullin, Emile Antat',
+  author_email = 'denisabdullincz@gmail.com, eoea754@gmail.com',
   description = 'Automated documentation engine for scikit-learn models',
-  long_description = long_description,
   long_description_content_type = 'text/markdown',
-  py_modules = ['placeholder'
-    ],
-  package_dir = {'': 'src'},
+  long_description = long_description,
+  packages = find_packages(),
   install_requires = ['pandas >= 1.0.0', 'scikit-learn >= 1.0.0', 'quarto', 'papermill >= 2.0.0'],
   extras_require = {'dev': ['pytest >= 3.7']},
   keywords = ['python', 'scikit-learn', 'sklearn', 'machine learning', 'documentation'],
   classifiers = [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3.10",
-    "Programming Language :: Python :: 3.11",
-    "Operating System :: OS Independent"
+    "Operating System :: Unix",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows"
   ]
 )
